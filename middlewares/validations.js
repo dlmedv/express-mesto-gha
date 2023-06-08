7ec00f4f5c03c1a2d2f8abed2fabe282ac0376b1
dlmedv <114763924+dlmedv@users.noreply.github.com>
2023-06-08 15:41:29 +0300

    реализация 14

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -2,14 +2,14 @@ const { celebrate, Joi } = require('celebrate');
 
 const validateSignIn = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/i),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 });
 
 const validateSignUp = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().pattern(/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/i),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
